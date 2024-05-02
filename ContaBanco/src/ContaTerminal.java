49d73bd0b35cc6260c96855ac380c7667d07005a
Renan <163042482+Renan-de-Jesus@users.noreply.github.com>
2024-05-02 09:25:10 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,24 +1,27 @@
+import java.util.Locale;
 import java.util.Scanner;
 
-public class ContaTerminal {
+public class Primeiro {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Olá, seja bem-vindo ao banco X!");
         System.out.print("Por favor, digite o seu nome: ");
         String nome = entrada.nextLine();
 
-        System.out.print("Digite o númeor da sua conta: ");
+        System.out.print("Digite o número da sua conta: ");
         int conta = entrada.nextInt();
+        
+        entrada.nextLine(); // Consumir o caractere de nova linha
 
         System.out.print("Digite o número da sua agência: ");
-        String agencia = entrada.next();
+        String agencia = entrada.nextLine();
 
         System.out.print("Digite o saldo da sua conta: ");
         double saldo = entrada.nextDouble();
 
-        System.out.println("Olá, " + nome + ", sua conta " +
-                conta + " da agência " + agencia + " tem saldo de R$ " + saldo);
+        System.out.println("Olá, " + nome + ", sua conta " + conta + " da agência " + agencia + " tem saldo de R$ " + saldo);
 
         entrada.close();
     }
